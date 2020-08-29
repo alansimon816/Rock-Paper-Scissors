45f288298c671e019f8aa1be58b842f041a0f15c
Alan <alansimon816@gmail.com>
2020-08-28 20:10:34 -0400

    Implements a 'start game' splash screen.

--- rps-gui.js
+++ rps-gui.js
@@ -1,3 +1,7 @@
+const ROCK = document.querySelector('#rock')
+const PAPER = document.querySelector('#paper')
+const SCISSORS = document.querySelector('#scissors')
+
 // returns a random integer between min (inclusive) and max (exclusive)
 let getRandom = (min, max) => ~~(Math.random() * (max - min) + min)
 
@@ -10,19 +14,29 @@ function computerPlay() {
     }
 }
 
-// simulates a round of rock, paper, scissors and determines if user won or lost
-function playRound(computerSelection, playerSelection) {
-    if (computerSelection == 'Rock' && playerSelection.match(SCISSORS)) {
+// plays an animation upon click
+function playClickAnimation(e) {
+    button.classList.add('playing')
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing'); //this refers to the button element calling addEventListener
+} 
+
+  // simulates a round of rock, paper, scissors and determines if user won or lost
+  function determineRound(computerSelection) {
+    if (computerSelection == 'Rock' && button.id === 'scissors') {
         return 'You lose the round! Rock beats scissors' 
-    } else if (computerSelection == 'Scissors' && playerSelection.match(PAPER)) {
+    } else if (computerSelection == 'Scissors' && button.id === 'paper') {
         return 'You lose the round! Scissors beats paper' 
-    } else if (computerSelection == 'Paper' && playerSelection.match(ROCK)) {
+    } else if (computerSelection == 'Paper' && button.id === 'rock') {
         return 'You lose the round! Paper beats rock'
-    } else if (computerSelection == 'Rock' && playerSelection.match(PAPER)) {
+    } else if (computerSelection == 'Rock' && button.id === 'paper') {
         return 'You win the round! Paper beats rock' 
-    } else if (computerSelection == 'Scissors' && playerSelection.match(ROCK)) {
+    } else if (computerSelection == 'Scissors' && button.id === 'rock') {
         return 'You win the round! Rock beats scissors' 
-    } else if (computerSelection == 'Paper' && playerSelection.match(SCISSORS)) {
+    } else if (computerSelection == 'Paper' && button.id === 'scissors') {
         return 'You win the round! Scissors beats paper'
     } else {
         return 'The round is a draw!' 
@@ -32,16 +46,21 @@ function playRound(computerSelection, playerSelection) {
 /* simulates 5 rounds of rock, paper, scissors between the computer and the user.
    The winner is whoever has won the most rounds by the end of the final round. */
 function game() {
-    // let roundResult, playerSelection, computerSelection, playerScore = 0, computerScore = 0, roundCount = 0
+    let roundResult, playerSelection, computerSelection, playerScore = 0, computerScore = 0, roundCount = 0
+    const buttons = document.querySelectorAll('button')
+
+    buttons.forEach(button => button.addEventListener('click', playClickAnimation))
+    computerSelection = computerPlay()
+    buttons.forEach(button => button.addEventListener('click', determineRound))
+    buttons.forEach(button => button.addEventListener('transitionend', removeTransition))
+
 
     // do {
-    //     playerSelection = prompt('Enter "Rock", "Paper", or "Scissors":' )
-    //     if (!isValid(playerSelection)) continue
     //     computerSelection = computerPlay()
     //     console.log(roundResult = playRound(computerSelection,playerSelection))
     //     roundResult.match(/win/) ? playerScore++ : computerScore++
     //     roundCount++
-    // } while (roundCount < 5)
+    // } while (roundCount == 0 /* < 5 */)
 
     // console.log(playerScore > computerScore ? 'You win the game!' : 'You lose the game!')
 }
