5e112c91bca299b1afec2ae6a875c5e9f4b40a73
Alan <alansimon816@gmail.com>
2020-12-21 00:49:40 -0500

    Stripped the design, improved functionality, added end game screen

--- rps-gui.js
+++ rps-gui.js
@@ -3,21 +3,13 @@ const PAPER = document.querySelector('#paper')
 const SCISSORS = document.querySelector('#scissors')
 const startBtn = document.querySelector("#start-button")
 
+let playerScore = 0, computerScore = 0, roundCount = 0
+
 // returns a random integer between min (inclusive) and max (exclusive)
 let getRandom = (min, max) => ~~(Math.random() * (max - min) + min)
 
-// plays an animation upon click
-function playClickAnimation(e) {
-    button.classList.add('playing')
-}
-
 startBtn.addEventListener('click', changeScreen)
 startBtn.addEventListener('click', playGame)
-
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    this.classList.remove('playing'); //this refers to the button element calling addEventListener
-} 
 
 // simulates a computer playing rock paper scissors
 function computerPlay() {
@@ -28,25 +20,42 @@ function computerPlay() {
     }
 }
 
-  // simulates a round of rock, paper, scissors and determines if user won or lost
-function determineRound(computerSelection) {
-    if (computerSelection == 'Rock' && button.id === 'scissors') {
-        return 'You lose the round! Rock beats scissors' 
-    } else if (computerSelection == 'Scissors' && button.id === 'paper') {
-        return 'You lose the round! Scissors beats paper' 
-    } else if (computerSelection == 'Paper' && button.id === 'rock') {
-        return 'You lose the round! Paper beats rock'
-    } else if (computerSelection == 'Rock' && button.id === 'paper') {
-        return 'You win the round! Paper beats rock' 
-    } else if (computerSelection == 'Scissors' && button.id === 'rock') {
-        return 'You win the round! Rock beats scissors' 
-    } else if (computerSelection == 'Paper' && button.id === 'scissors') {
-        return 'You win the round! Scissors beats paper'
+// simulates a round of rock, paper, scissors and determines if user won or lost
+function determineRound(e) {
+    computerSelection = computerPlay()
+    console.log(this.id)
+    let roundResult, gameResult
+    if (computerSelection === 'Rock' && this.id == 'scissors') {
+        roundResult = 'You lose the round! Rock beats scissors' 
+    } else if (computerSelection === 'Scissors' && this.id == 'paper') {
+        roundResult = 'You lose the round! Scissors beats paper'
+    } else if (computerSelection === 'Paper' && this.id == 'rock') {
+        roundResult = 'You lose the round! Paper beats rock'
+    } else if (computerSelection === 'Rock' && this.id == 'paper') {
+        roundResult = 'You win the round! Paper beats rock'
+    } else if (computerSelection === 'Scissors' && this.id == 'rock') {
+        roundResult = 'You win the round! Rock beats scissors' 
+    } else if (computerSelection === 'Paper' && this.id == 'scissors') {
+        roundResult = 'You win the round! Scissors beats paper'
     } else {
-        return 'The round is a draw!' 
-    }     
+        roundResult = 'The round is a draw!'
+        console.log(roundResult)
+        return
+    } 
+    roundResult.match(/win/) ? playerScore++ : computerScore++  
+    console.log(roundResult)
+    console.log('Player: ' + playerScore + '     CPU: ' + computerScore) 
+    roundCount++
+    if (roundCount === 5) {
+        console.log(gameResult = (playerScore > computerScore) ? 'You win the game!' : 'You lose the game!')
+        document.getElementById("end-text").textContent = gameResult
+        // Now, hide game screen and go to new screen with Win/Loss message!
+        game.style.visibility = 'hidden'
+        window['end-screen'].style.visibility = 'visible'
+    }  
 }
 
+// Changes splash screen to game screen with game buttons
 function changeScreen () {
     startScreen = document.querySelector('#start-screen')
     game = document.querySelector('#game')
@@ -57,24 +66,9 @@ function changeScreen () {
 
 /* simulates 5 rounds of rock, paper, scissors between the computer and the user.
    The winner is whoever has won the most rounds by the end of the final round.
-   
    We want this function to be executed when the Start Game button is clicked*/
 function playGame() {
-    let roundResult, playerSelection, computerSelection, playerScore = 0, computerScore = 0, roundCount = 0
     const buttons = document.querySelectorAll('button')
-
-    buttons.forEach(button => button.addEventListener('click', playClickAnimation))
-    computerSelection = computerPlay()
-    buttons.forEach(button => button.addEventListener('click', determineRound))
-    buttons.forEach(button => button.addEventListener('transitionend', removeTransition))
-
-
-    // do {
-    //     computerSelection = computerPlay()
-    //     console.log(roundResult = playRound(computerSelection,playerSelection))
-    //     roundResult.match(/win/) ? playerScore++ : computerScore++
-    //     roundCount++
-    // } while (roundCount == 0 /* < 5 */)
-
-    // console.log(playerScore > computerScore ? 'You win the game!' : 'You lose the game!')
+    const game_buttons = document.querySelectorAll('.game-buttons')
+    game_buttons.forEach(button => button.addEventListener('click', determineRound))
 }
